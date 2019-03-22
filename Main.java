cbe4f206b5b8c182491f48184945f4865ed1de12
brittanyabrahams2 <43973188+brittanyabrahams2@users.noreply.github.com>
2019-03-22 16:24:47 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+            int result = 1 + 2;
+        System.out.println(result);
+
+        int prevResult = result;
+        result = result -1;
+        System.out.println(prevResult + "- 1 =" + result);
+        prevResult = result;
+        result = result * 10;
+        System.out.println(prevResult + "*10 =" + result);
+
+        boolean isFake = false;
+        if(isFake = true)
+            System.out.println("isnt a fake ass bitch");
+
+    int newValue = 50;
+    if(newValue == 50)
+        System.out.println("This is true");
+
+     double doubleVar = 20;
+     double secDoubleVar = 80;
+     double sum = (doubleVar + secDoubleVar) * 25;
+     double rem = (sum % 40);
+
+     if(rem <= 20)
+         System.out.println("Total was over the limit");
+
+    }
+}
